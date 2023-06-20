4cfa835fa471807f546996f6ed093821f0332c45
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-06-20 23:10:13 +0300

    non-repeating countries, highlight guessed ones

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required, apology
 import sqlite3
+import random
 
 # Create database or create it if it does not exist
 conn = sqlite3.connect('geofact.db')
@@ -22,8 +23,43 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS users (
 # Configure application
 app = Flask(__name__)
 
-countries = [
-    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", 
+popular_countries = ["Argentina", "Armenia", "Australia", "Austria", "Belgium", "Brazil", 
+                     "Canada", "Chile", "China", "Czech Republic", "Denmark", "Finland", 
+                     "France", "Germany", "Greece", "Hungary", "Iceland", "India", "Indonesia", 
+                     "North Korea", "South Korea", "Mexico", "Mongolia", "Netherlands", "New Zealand", 
+                     "Norway", "Pakistan", "Poland", "Portugal", "Russian Federation", "Saudi Arabia", "Slovakia", 
+                     "Slovenia", "Spain", "Sweden", "Switzerland", "Syria", "South Africa", "Venezuela", 
+                     "Vietnam", "United States", "United Kingdom", "Ukraine", "Turkey", "Iran", "Iraq", 
+                     "Ireland", "Israel", "Italy", "Japan", "Latvia", "Lithuania", "Romania"]
+
+less_popular_countries = ["Afghanistan", "Albania", "Algeria", "Azerbaijan", "Bangladesh", "Belarus", 
+                          "Belize", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bulgaria", 
+                          "Cambodia", "Cameroon", "Central African Republic", "Chad", "Colombia", 
+                          "Congo", "Croatia", "Cuba", "Ecuador", "Egypt", "El Salvador", "Estonia", 
+                          "Ethiopia", "Georgia", "Ghana", "Guatemala", "Honduras", "Jamaica", "Jordan", 
+                          "Kazakhstan", "Kenya", "Kyrgyzstan", "Kosovo", "Kuwait", "Laos", "Lebanon", 
+                          "Liberia", "Libya", "Luxembourg", "Madagascar", "Malaysia", "Moldova", "Monaco", 
+                          "Montenegro", "Morocco", "Namibia", "Nepal", "Nicaragua", "Nigeria", 
+                          "North Macedonia", "Panama", "Papua New Guinea", "Paraguay", "Peru", 
+                          "Philippines", "Qatar", "Serbia", "Zambia", "Zimbabwe", "United Arab Emirates", 
+                          "Uzbekistan", "Yemen", "Singapore", "Somalia", "Sri Lanka", "Taiwan", 
+                          "Tajikistan", "Tanzania", "Thailand", "Tunisia", "Turkmenistan", "Uganda", "Uruguay"]
+
+
+least_popular_countries = ["Andorra", "Angola", "Antigua and Barbuda", "Bahamas", "Bahrain", "Barbados", "Benin", 
+                           "Bhutan", "Brunei", "Burkina Faso", "Burundi", "Cabo Verde", "Comoros", "Costa Rica", 
+                           "Cyprus", "Djibouti", "Dominica", "Dominican Republic", "Equatorial Guinea", "Eritrea", 
+                           "Eswatini", "Fiji", "Gabon", "Gambia", "Grenada", "Guinea", "Guinea-Bissau", "Guyana", 
+                           "Haiti", "Kiribati", "Lesotho", "Liechtenstein", "Malawi", "Maldives", "Mali", "Malta", 
+                           "Marshall Islands", "Mauritania", "Mauritius", "Micronesia", "Mozambique", "Myanmar", 
+                           "Nauru", "Niger", "Oman", "Palau", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", 
+                           "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", 
+                           "Senegal", "Seychelles", "Vatican City", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", 
+                           "Suriname", "Sierra Leone", "Solomon Islands", "South Sudan", "Sudan", "Tuvalu", "Vanuatu"]
+
+def get_random_country():
+    guessed_countries = session.get("guessed_countries", [])
+    all_countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", 
     "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", 
     "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", 
     "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", 
@@ -33,25 +69,26 @@ countries = [
     "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", 
     "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", 
     "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", 
-    "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", 
+    "Kenya", "Kiribati", "North Korea", "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", 
     "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", 
     "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", 
     "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", 
     "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", 
     "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", 
-    "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", 
+    "Poland", "Portugal", "Qatar", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", 
     "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", 
     "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", 
     "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", 
     "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", 
     "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", 
     "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", 
-    "Zambia", "Zimbabwe"
-]
-
-def get_random_country():
-    import random
-    return random.choice(countries)
+    "Zambia", "Zimbabwe"]  # List of all countries
+    available_countries = [country for country in all_countries if country not in guessed_countries]
+    
+    if available_countries:
+        return random.choice(available_countries)
+    else:
+        return None
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -196,6 +233,7 @@ def logout():
 def play():
     country = get_random_country()
     session["score"] = 0
+    session["guessed_countries"] = []
     return render_template("mode1.html", country=country)
 
 @app.route("/process_country", methods=["POST"])
@@ -205,9 +243,11 @@ def process_country():
     result = "❌" if guessed_country != random_country else "✅"
     if guessed_country == random_country:
         session["score"] += 1
+        session["guessed_countries"].append(guessed_country)
     country = get_random_country()
     session["random_country"] = country  # Store the new random country in the session
     score = session.get("score")
-    return render_template("mode1.html", country=country, random_country=random_country, result=result, score=score)
+    guessed_countries = session.get("guessed_countries")
+    return render_template("mode1.html", country=country, random_country=random_country, result=result, score=score, guessed_countries=guessed_countries)
 
 
