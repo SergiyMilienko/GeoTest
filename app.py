0c42d3f22b9b4b609b84321d96ed8f636460710b
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-06-21 13:54:50 +0300

    game mode, end of game, final score

--- app.py
+++ app.py
@@ -30,15 +30,16 @@ popular_countries = ["Argentina", "Armenia", "Australia", "Austria", "Belgium", 
                      "Norway", "Pakistan", "Poland", "Portugal", "Russian Federation", "Saudi Arabia", "Slovakia", 
                      "Slovenia", "Spain", "Sweden", "Switzerland", "Syria", "South Africa", "Venezuela", 
                      "Vietnam", "United States", "United Kingdom", "Ukraine", "Turkey", "Iran", "Iraq", 
-                     "Ireland", "Israel", "Italy", "Japan", "Latvia", "Lithuania", "Romania"]
+                     "Ireland", "Israel", "Italy", "Japan", "Latvia", "Lithuania", "Romania", "Kazakhstan", 
+                     "Estonia", "Belarus"]
 
-less_popular_countries = ["Afghanistan", "Albania", "Algeria", "Azerbaijan", "Bangladesh", "Belarus", 
+less_popular_countries = ["Afghanistan", "Albania", "Algeria", "Azerbaijan", "Bangladesh",
                           "Belize", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bulgaria", 
                           "Cambodia", "Cameroon", "Central African Republic", "Chad", "Colombia", 
-                          "Congo", "Croatia", "Cuba", "Ecuador", "Egypt", "El Salvador", "Estonia", 
+                          "Congo", "Croatia", "Cuba", "Ecuador", "Egypt", "El Salvador", 
                           "Ethiopia", "Georgia", "Ghana", "Guatemala", "Honduras", "Jamaica", "Jordan", 
-                          "Kazakhstan", "Kenya", "Kyrgyzstan", "Kosovo", "Kuwait", "Laos", "Lebanon", 
-                          "Liberia", "Libya", "Luxembourg", "Madagascar", "Malaysia", "Moldova", "Monaco", 
+                          "Kenya", "Kyrgyzstan", "Kosovo", "Kuwait", "Laos", "Lebanon", 
+                          "Liberia", "Libya", "Luxembourg", "Madagascar", "Malaysia", "Moldova", 
                           "Montenegro", "Morocco", "Namibia", "Nepal", "Nicaragua", "Nigeria", 
                           "North Macedonia", "Panama", "Papua New Guinea", "Paraguay", "Peru", 
                           "Philippines", "Qatar", "Serbia", "Zambia", "Zimbabwe", "United Arab Emirates", 
@@ -46,20 +47,18 @@ less_popular_countries = ["Afghanistan", "Albania", "Algeria", "Azerbaijan", "Ba
                           "Tajikistan", "Tanzania", "Thailand", "Tunisia", "Turkmenistan", "Uganda", "Uruguay"]
 
 
-least_popular_countries = ["Andorra", "Angola", "Antigua and Barbuda", "Bahamas", "Bahrain", "Barbados", "Benin", 
+least_popular_countries = ["Angola", "Antigua and Barbuda", "Bahamas", "Bahrain", "Barbados", "Benin", 
                            "Bhutan", "Brunei", "Burkina Faso", "Burundi", "Cabo Verde", "Comoros", "Costa Rica", 
                            "Cyprus", "Djibouti", "Dominica", "Dominican Republic", "Equatorial Guinea", "Eritrea", 
                            "Eswatini", "Fiji", "Gabon", "Gambia", "Grenada", "Guinea", "Guinea-Bissau", "Guyana", 
-                           "Haiti", "Kiribati", "Lesotho", "Liechtenstein", "Malawi", "Maldives", "Mali", "Malta", 
+                           "Haiti", "Kiribati", "Lesotho", "Malawi", "Maldives", "Mali", "Malta", 
                            "Marshall Islands", "Mauritania", "Mauritius", "Micronesia", "Mozambique", "Myanmar", 
                            "Nauru", "Niger", "Oman", "Palau", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", 
-                           "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", 
-                           "Senegal", "Seychelles", "Vatican City", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", 
+                           "Saint Vincent and the Grenadines", "Samoa", "Sao Tome and Principe", 
+                           "Senegal", "Seychelles", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", 
                            "Suriname", "Sierra Leone", "Solomon Islands", "South Sudan", "Sudan", "Tuvalu", "Vanuatu"]
 
-def get_random_country():
-    guessed_countries = session.get("guessed_countries", [])
-    all_countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", 
+all_countries = ["Afghanistan", "Albania", "Algeria", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", 
     "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", 
     "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", 
     "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", 
@@ -70,21 +69,110 @@ def get_random_country():
     "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", 
     "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", 
     "Kenya", "Kiribati", "North Korea", "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", 
-    "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", 
+    "Lebanon", "Lesotho", "Liberia", "Libya", "Lithuania", "Luxembourg", "Madagascar", 
     "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", 
-    "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", 
+    "Mexico", "Micronesia", "Moldova", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", 
     "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", 
     "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", 
     "Poland", "Portugal", "Qatar", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", 
-    "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", 
+    "Saint Vincent and the Grenadines", "Samoa", "Sao Tome and Principe", "Saudi Arabia", 
     "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", 
     "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", 
     "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", 
     "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", 
-    "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", 
-    "Zambia", "Zimbabwe"]  # List of all countries
+    "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", 
+    "Zambia", "Zimbabwe"]
+
+africa = [
+    "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi",
+    "Cabo Verde", "Cameroon", "Central African Republic", "Chad", "Comoros",
+    "Congo", "Côte d'Ivoire", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea",
+    "Eswatini", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau",
+    "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Mauritania",
+    "Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda",
+    "São Tomé and Príncipe", "Senegal", "Seychelles", "Sierra Leone", "Somalia",
+    "South Africa", "South Sudan", "Sudan", "Tanzania", "Togo", "Tunisia", "Uganda",
+    "Zambia", "Zimbabwe"
+]
+
+asia = [
+    "Afghanistan", "Armenia", "Azerbaijan", "Bahrain", "Bangladesh", "Bhutan",
+    "Brunei", "Cambodia", "China", "Cyprus", "Georgia", "India", "Indonesia",
+    "Iran", "Iraq", "Israel", "Japan", "Jordan", "Kazakhstan", "Kuwait", "Kyrgyzstan",
+    "Laos", "Lebanon", "Malaysia", "Maldives", "Mongolia", "Myanmar", "Nepal",
+    "North Korea", "Oman", "Pakistan", "Palestine", "Philippines", "Qatar", "Russia",
+    "Saudi Arabia", "Singapore", "South Korea", "Sri Lanka", "Syria", "Taiwan",
+    "Tajikistan", "Thailand", "Timor-Leste", "Turkey", "Turkmenistan", "United Arab Emirates",
+    "Uzbekistan", "Vietnam", "Yemen"
+]
+
+europe = [
+    "Albania", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina",
+    "Bulgaria", "Croatia", "Czech Republic", "Denmark", "Estonia", "Finland", "France",
+    "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kosovo", "Latvia",
+    "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro",
+    "Netherlands", "North Macedonia", "Norway", "Poland", "Portugal", "Romania",
+    "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine",
+    "United Kingdom"
+]
+
+north_america = [
+    "Antigua and Barbuda", "Bahamas", "Barbados", "Belize", "Canada", "Costa Rica",
+    "Cuba", "Dominica", "Dominican Republic", "El Salvador", "Grenada", "Guatemala",
+    "Haiti", "Honduras", "Jamaica", "Mexico", "Nicaragua", "Panama", "Saint Kitts and Nevis",
+    "Saint Lucia", "Saint Vincent and the Grenadines", "Trinidad and Tobago", "United States"
+]
+
+oceania = [
+    "Australia", "Fiji", "Kiribati", "Marshall Islands", "Micronesia", "Nauru",
+    "New Zealand", "Palau", "Papua New Guinea", "Samoa", "Solomon Islands", "Tonga",
+    "Tuvalu", "Vanuatu"
+]
+
+south_america = [
+    "Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana",
+    "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"
+]
+
+def get_random_country():
+    difficulty = session.get("difficulty")
+    guessed_countries = session.get("guessed_countries", [])
     available_countries = [country for country in all_countries if country not in guessed_countries]
     
+    if difficulty == "easy":
+        available_countries = popular_countries
+        session["max_score"] = len(popular_countries)
+    elif difficulty == "medium":
+        available_countries = less_popular_countries
+        session["max_score"] = len(less_popular_countries)
+    elif difficulty == "hard":
+        available_countries = least_popular_countries
+        session["max_score"] = len(least_popular_countries)
+    elif difficulty == "all":
+        available_countries = all_countries
+        session["max_score"] = len(all_countries)
+    elif difficulty == "europe":
+        available_countries = europe
+        session["max_score"] = len(europe)
+    elif difficulty == "asia":
+        available_countries = asia
+        session["max_score"] = len(asia)
+    elif difficulty == "oceania":
+        available_countries = oceania
+        session["max_score"] = len(oceania)
+    elif difficulty == "north_america":
+        available_countries = north_america
+        session["max_score"] = len(north_america)
+    elif difficulty == "south_america":
+        available_countries = south_america
+        session["max_score"] = len(south_america)
+    elif difficulty == "africa":
+        available_countries = africa
+        session["max_score"] = len(africa)
+
+
+    available_countries = [country for country in available_countries if country not in guessed_countries]
+
     if available_countries:
         return random.choice(available_countries)
     else:
@@ -231,8 +319,13 @@ def logout():
     
 @app.route("/play", methods=["GET", "POST"])
 def play():
+    if request.method == "POST":
+        difficulty = request.form.get("difficulty")
+        session["difficulty"] = difficulty
+    
     country = get_random_country()
     session["score"] = 0
+    session["count"] = 0
     session["guessed_countries"] = []
     return render_template("mode1.html", country=country)
 
@@ -243,11 +336,16 @@ def process_country():
     result = "❌" if guessed_country != random_country else "✅"
     if guessed_country == random_country:
         session["score"] += 1
+        session["count"] += 1
         session["guessed_countries"].append(guessed_country)
+    else:
+        session["count"] += 1
     country = get_random_country()
     session["random_country"] = country  # Store the new random country in the session
     score = session.get("score")
+    count = session.get("count")
+    max_score = session.get("max_score")
     guessed_countries = session.get("guessed_countries")
-    return render_template("mode1.html", country=country, random_country=random_country, result=result, score=score, guessed_countries=guessed_countries)
+    return render_template("mode1.html", country=country, random_country=random_country, result=result, score=score, guessed_countries=guessed_countries, max_score=max_score, count=count)
 
 
